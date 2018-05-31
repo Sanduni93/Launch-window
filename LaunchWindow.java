76d0d904155d69c0e09742e07e545137e1af0817
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-05-31 20:48:37 +0530

    Primary stage of Javafx

---
+++ LaunchWindow.java
@@ -0,0 +1,22 @@
+package javafxapplication1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFXApplication1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+}
